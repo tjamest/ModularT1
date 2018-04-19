82c4685f746101e8332e1221e73c4d06fbc53c67
GustavoContreiras <guto.contreiras@gmail.com>
2018-04-18 21:49:01 -0300

    Create BARALHO.h

---
+++ BARALHO.h
@@ -0,0 +1,129 @@
+#if ! defined( BARALHO_ )
+#define BARALHO_
+/***************************************************************************
+*
+*  $MCD Módulo de definição: BAR  Baralho
+*
+*  Arquivo gerado:              BARALHO.h
+*  Letras identificadoras:      BAR
+*
+*  Nome da base de software:    Truco
+*  Arquivo da base de software:
+*
+*  Projeto: INF 1301 Jogo de Truco
+*  Gestor:
+*  Autores: gsc, jvr, tgf
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     1       gsc   19/abr/2018 início desenvolvimento
+*
+*  $ED Descrição do módulo
+*     Cria um baralho de 40 cartas (sem os 8, 9, 10 e coringa).
+*     Embaralha essas 40 cartas.
+*     Destrói esse baralho.
+***************************************************************************/
+
+#if defined( BARALHO_OWN )
+   #define BARALHO_EXT
+#else
+   #define BARALHO_EXT extern
+#endif
+
+/***** Declarações exportadas pelo módulo *****/
+
+/* Tipo referência para uma carta */
+
+typedef struct Carta * BAR_tpCarta
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: BAR Condições de retorno
+*
+*
+*  $ED Descrição do tipo
+*     Condições de retorno das funções de carta
+*
+***********************************************************************/
+
+typedef enum {
+
+  BAR_CondRetOK,              /* Concluiu corretamente */
+
+  BAR_CondRetFaltouMemoria,   /* Faltou memoria */
+  
+  } BAR_tpCondRet;
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: BAR tpValor
+*
+*
+*  $ED Descrição do tipo
+*     Domínio dos possíveis valores de uma carta.
+*
+***********************************************************************/
+typedef enum {
+
+  _4,               /* 0  */
+  _5,               /* 1  */
+  _6,               /* 2  */
+  _7,               /* 3  */
+  _J,               /* 4  */
+  _Q,               /* 5  */
+  _K,               /* 6  */
+  _2,               /* 7  */
+  _3,               /* 8  */
+  _ManilhaOuros,    /* 9  */
+  _ManilhaEspadas,  /* 10 */
+  _ManilhaCopas,    /* 11 */
+  _ManilhaPaus      /* 12 */
+  
+   } BAR_tpValor ;
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: BAR tpNaipe
+*
+*
+*  $ED Descrição do tipo
+*     Dominio dos possiveis naipes de uma carta.
+*
+***********************************************************************/
+typedef enum {
+
+  Ouros,
+  Espadas,
+  Copas,
+  Paus
+
+   } BAR_tpNaipe ;
+
+/***********************************************************************
+*
+*  $FC Função: LIS  &Criar Baralho
+*
+*  $ED Descrição da função
+*   Cria um baralho (lista) de 40 cartas sem os 8, 9, 10 e coringas.
+*
+*  $EP Parâmetros
+*     *pBaralho: Ponteiro para o início da lista (baralho)
+*
+*  $FV Valor retornado
+*     Se executou corretamente retorna o ponteiro para o início do baralho.
+*     Este ponteiro será utilizado pelas funções que manipulem este baralho.
+*
+*     Se ocorreu algum erro, por exemplo falta de memória ou dados errados,
+*     a função retornará NULL.
+*     Não será dada mais informação quanto ao problema ocorrido.
+*
+*     Assertivas de entrada:
+*     - O endereço de ponteiro para a carta deve ser válido
+*     - O valor deve estar dentro do intervalo definido (0 a 12)
+*     - O naipe deve ser válido
+*     Assertivas de saída:
+*     - O espaço de memória para o baralho foi alocado com sucesso e seus valores definidos corretamente
+*     
+*
+***********************************************************************/
+BAR_tpCondRet BAR_CriarBaralho(afadfagage);
