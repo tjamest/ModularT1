511ccf261a1290f93b5723bb44a606e11b0ce7e5
GustavoContreiras <guto.contreiras@gmail.com>
2018-04-23 16:49:24 -0300

    Update MESA.h

--- MESA.h
+++ MESA.h
@@ -1,1 +1,29 @@
+#if ! defined( MESA_ )
+#define MESA_
+/***************************************************************************
+*
+*  $MCD Módulo de definição: MES  MESA
+*
+*  Arquivo gerado:              MESA.h
+*  Letras identificadoras:      MES
+*
+*  Nome da base de software:    Truco
+*  Arquivo da base de software:
+*
+*  Projeto: INF 1301 Jogo de Truco
+*  Gestor:
+*  Autores: gsc, jvr, tgf
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     1       gsc   19/abr/2018 início desenvolvimento
+*
+*  $ED Descrição do módulo
+*     .
+***************************************************************************/
 
+#if defined( MESA_OWN )
+   #define MESA_EXT
+#else
+   #define MESA_EXT extern
+#endif
