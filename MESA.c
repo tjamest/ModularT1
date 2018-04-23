3e7d3ccf3d5c4f39ad1368ba12386af0941baa1b
GustavoContreiras <guto.contreiras@gmail.com>
2018-04-23 16:47:15 -0300

    Update MESA.c

--- MESA.c
+++ MESA.c
@@ -1,1 +1,17 @@
-
+/***************************************************************************
+*  $MCI Módulo de implementação: MES  Mesa
+*
+*  Arquivo gerado:              MESA.c
+*  Letras identificadoras:      MES
+*
+*  Nome da base de software:    Truco
+*  Arquivo da base de software: 
+*
+*  Projeto: INF 1301 - Jogo de Truco
+*  Autores: gsc, jvr, tgf
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     1       avs   23/abr/2018 início desenvolvimento
+*
+***************************************************************************/
