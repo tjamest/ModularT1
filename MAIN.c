610b10ccef9fa98a7d404b1540231821bd4cfd9d
GustavoContreiras <guto.contreiras@gmail.com>
2018-05-05 01:08:06 -0300

    Add files via upload

---
+++ MAIN.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "LISTA.h"
+#include "BARALHO.h"
+
+int main (void) {
+
+	LIS_tppLista pCabecaBaralho;
+
+	pCabecaBaralho = BAR_CriarBaralho () ;
+
+	return 0;
+}
